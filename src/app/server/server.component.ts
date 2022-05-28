b75b38b2e495ad951804983d6ad4fc8853f68fb3
Yoeri J. Moens <yoerimoens@gmail.com>
2022-05-29 00:50:14 +0200

    Eslint formatting

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css'],
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent {
+    public serverId: number = 10
+    public serverStatus: string = 'offline'
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    public getServerStatus() {
+        return this.serverStatus
+    }
 }
